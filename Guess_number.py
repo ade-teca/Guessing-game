aa3435584f3380d7f289fd036a3645e355638a57
ade-teca <adelitoteca@gmail.com>
2024-04-15 17:42:30 -0300

    Added screen clean and repeat the game question

--- Guess_number.py
+++ Guess_number.py
@@ -1,18 +1,47 @@
+import os
 import random
 
-start_number = int(input("start number: "))
-end_number = int(input("end number: "))
+def clear_console():
+    # Clear the console
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+#guess function
 def guess(x, y):
     random_number = random.randint(x, y)   
-    user_input = -2
+    user_input = random_number-1
+    clear_console()
 
+    trying = 0
     while user_input != random_number:
-        user_input = int(input(f"Guess the number between {x} and {y}: "))
+        user_input = int(input(f"\nGuess the number between {x} and {y}: "))
         if user_input < random_number:
-            print(" Too low")
+            print("\n   Too low\n")
         elif user_input > random_number:
-            print("Too high")
-    print(f"Well done! The number {random_number} is correct.")
+            print("\n   Too high")
+        trying += 1
+    else:
+        clear_console()
+        print(f"\n  Well done! The number {random_number} is correct.")
+        print(f"    You did it in {trying} tryings.")
+        trying += 1
 
-guess(start_number, end_number)
+#welcome massage
+clear_console()
+print("#"*30)
+print("WELCOME TO GUESS-NUMBER-GAME")
+print("#"*30)
+print("\n")
+
+#Call the function to guess the number
+while True:
+    #Starting the game?
+    start_game = input("Do you wanna play? (S/N) : ")
+    if start_game.upper() != "S":
+        clear_console()
+        print("\n    Good bye, maybe another time...\n")
+        quit()
+    print("\nGreat decision, let´s getting start.")
+    start_number = int(input("start number: "))
+    end_number = int(input("end number: "))
+    guess(start_number, end_number)
+    
